6c564118e29cfc6e9d1d5cdc9b4660c738ad318e
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-09-22 15:45:07 -0300

    Projeto Jogo da Forca

---
+++ Js/canvas.js
@@ -0,0 +1,74 @@
+let tela = document.querySelector('canvas');
+let pincel = tela.getContext('2d');
+pincel.fillStyle = "rgb(239, 241, 245)";
+pincel.fillRect(20, 0, 800, 400);
+
+function desenhaCirculo(x, y, raio, cor) {
+
+  pincel.fillStyle = cor;
+  pincel.beginPath();
+  pincel.arc(x, y, raio, 0, 2 * 3.14);
+  pincel.fill();
+}
+
+function desenhaRetangulo(x, y, tamanho1, tamanho2, cor){
+
+    pincel.fillStyle = cor;
+    pincel.fillRect(x , y, tamanho1, tamanho2);
+    pincel.fillStroke = cor;
+    pincel.strokeRect(x , y, tamanho1, tamanho2);  
+}
+
+function desenhaBracoD(){
+  pincel.fillStyle = 'brown';
+  pincel.beginPath();
+  pincel.moveTo(180, 180);
+  pincel.lineTo(142, 200);  
+  pincel.stroke();
+}
+
+function desenhaBracoE(){
+  pincel.fillStyle = 'brown';
+  pincel.beginPath();
+  pincel.moveTo(215, 205 );
+  pincel.lineTo(180, 180);  
+  pincel.stroke();
+}
+
+function desenhaPernaE(){
+  pincel.fillStyle = 'brown';
+  pincel.beginPath();
+  pincel.moveTo(220, 265 );
+  pincel.lineTo(183, 228);  
+  pincel.stroke();
+}
+
+function desenhaPernaD(){
+  pincel.fillStyle = 'brown';
+  pincel.beginPath();
+  pincel.moveTo(176, 228 );
+  pincel.lineTo(145, 270);  
+  pincel.stroke();
+}
+
+desenhaRetangulo(60, 70, 20, 330, "brown");
+desenhaRetangulo(50, 80, 180, 20, "brown");
+desenhaRetangulo(180, 99, 2, 60, "brown");
+
+
+function desenha(erros){
+  if(erros === 1){
+    desenhaCirculo(180, 150, 18, "black")
+    desenhaCirculo(180, 150, 16, "white")
+  } else if(erros === 2){
+    desenhaRetangulo(178, 168, 3, 60, "black");
+  } else if(erros === 3){
+    desenhaBracoD()
+  }else if(erros === 4){
+    desenhaBracoE()
+  }else if(erros === 5){
+    desenhaPernaE()
+  }else if(erros === 6){
+    desenhaPernaD()
+  }
+}
---
+++ Js/index.js
@@ -0,0 +1,143 @@
+//letiaáveis
+let palavras = ["BANANA", "ANA", "ALURA",
+  "CIDADE", "RADIO", "CARRO",
+  "FAZENDA", "AVENIDA", "UNIDADE",
+  "SECULO", "SALADA", "MEDO",
+  "PASSARO", "URSO", "MEL", 'CHAPEU',
+  'CHAVE', 'EXPORTAR', 'ORACLE', 'JAVA'];
+let palavraSorteada = [];
+let contador = 0;
+let novaPalavra;
+let botaoIniciarJogo = document.querySelector('.blue');
+let modal = document.querySelector(".modal");
+let input = document.querySelector('.input-letras');
+let inputLetra = document.createElement('input');
+let botaoAdicionaPalavra = document.querySelector(".white")
+let erros = 0;
+let acertos = 0;
+let botaoNovoJogo = document.querySelector("#start")
+let exibeGameOver = document.querySelector("#over")
+let exibeWin = document.querySelector("#win")
+let botaoWin = document.querySelector("#winB")
+
+//Teclado
+
+// letras = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+
+//Jogo
+
+
+botaoIniciarJogo.addEventListener('click', function startGame(event) {
+
+  event.preventDefault();
+
+  palavraSorteada = [];
+
+  modal.classList.remove("modal");
+  modal.style.zIndex = -3;
+  box.style.zIndex = -3
+  sorteiaPalavra();
+  dividePalavra();
+  criaInputLetra();
+
+}, false);
+
+
+//Functions
+
+function sorteiaPalavra() {
+  while (palavraSorteada.length < 1) {
+    let i = Math.floor(Math.random() * palavras.length);
+    if (palavraSorteada.indexOf(palavras[i]) < 0) {
+      palavraSorteada.push(palavras[i])
+    }
+  }
+  return palavraSorteada;
+}
+
+function dividePalavra() {
+
+  novaPalavra = palavraSorteada[0].split("");
+
+}
+
+function criaInputLetra() {
+
+  for (let i = 0; i < novaPalavra.length; i++) {
+    let inputLetra = document.createElement('input');
+    let div = document.getElementById('msg');
+    inputLetra.classList.add("input-palavra");
+    div.appendChild(inputLetra);
+
+  }
+}
+
+botaoAdicionaPalavra.addEventListener("click", function adcionarPalavra(event) {
+
+  event.preventDefault();
+
+  let inputPalavra = document.querySelector("#palavra-digitada");
+  let palavraDigitada = inputPalavra.value;
+  palavraDigitada = palavraDigitada.toUpperCase();
+  palavras.push(palavraDigitada)
+  document.querySelector('#palavra-digitada').value = palavraDigitada;
+
+  inputPalavra.value = "";
+
+})
+
+function gameOver() {
+  exibeGameOver.style.zIndex = 3;
+  botaoNovoJogo.addEventListener('click', function () {
+    document.location.reload(true);
+
+  }, false)
+}
+
+function gameWin() {
+  exibeWin.style.zIndex = 3;
+  botaoWin.addEventListener('click', function () {
+    document.location.reload(true);
+  }, false)
+}
+
+
+let letraClicada = document.querySelectorAll('.button-letra')
+let divs = document.querySelector('#butons-letras')
+
+divs.addEventListener('click', (event) => {
+
+  if (event.target.textContent.length > 1) return;
+
+  letra = event.target.textContent;
+
+  let inputAll = document.querySelectorAll(".input-palavra");
+
+  let valida = true;
+
+
+  for (i = 0; i < novaPalavra.length; i++) {
+    if (letra === novaPalavra[i]) {
+      valida = false;
+      inputAll[i].value = letra;
+      event.target.style.backgroundColor = "rgba(14, 238, 6, 0.89)"
+      acertos++;
+
+      if (acertos === novaPalavra.length) {
+        gameWin();
+
+      }
+
+    }
+  }
+
+  if (valida) {
+    event.target.style.backgroundColor = "rgba(255, 0, 0, 0.89)"
+    erros++;
+    desenha(erros);
+    if (erros === 6) {
+      gameOver();
+    }
+  }
+
+}, false);
